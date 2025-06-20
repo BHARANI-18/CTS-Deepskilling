eb796bb48f26fb05e057748eac3e43b56c6d5c9d
BHARANI S <136556456+BHARANI-18@users.noreply.github.com>
2025-06-20 17:18:58 +0530

    Update Main.java

--- Week-1/FinancialForecasting/Main.java
+++ Week-1/FinancialForecasting/Main.java
@@ -3,7 +3,7 @@ package com.forecasting;
 public class Main {
     public static void main(String[] args) {
         double presentValue = 10000.0;
-        double growthRate = 0.08; // 8%
+        double growthRate = 0.08; 
         int years = 5;
 
         double futureValueRecursive = ForecastCalculator.forecastRecursive(presentValue, growthRate, years);
